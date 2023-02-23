67d773d9e0ff5f9e3164935a8dad8ca7e83e0567
momenur <momenurislam6@gmail.com>
2023-02-23 11:46:57 +0600

    added summary

---
+++ summary.js
@@ -0,0 +1,39 @@
+/*
+ ================   MUST BE KNOW   ==================
+ 1. difference with var,let,Constant .
+ 2. Template String
+    2.1: Create Multi-Line String (` `).
+    2.2: Add Dynamic Variable (Expression) : `${Your Dynamic Variable}`
+ 3. Arrow Function 
+    3.1: Single Parameter Arrow Function.
+    3.2: Two Parameter Arrow Function.  
+    3.3: Arrow Function With Function Body.
+        And Don't Forget you have to return explicity. 
+4. Map, Filter, Find (You Must Have to know map, filter, find properly).
+5. Optional Chaining 
+
+
+
+
+ ================   GOOD TO HAVE   ==================
+ 1. Spread Operator: [....numbers, 30, 40];
+ 2. Destructuring 
+    2.1: Object Destructuring . How to get properly name as Variable .
+        const {name} = {
+            name: 'Orange',
+            color: 'Orange Color'
+        }
+    2.2: Array Destructing . 
+        const [a, b, c] = [20, 40, 10, 50, 60];
+3. Default Parameter 
+4. forEach
+5. Dot Notation VS bracket Notation 
+
+
+
+ ================   SHOULD KNOW AS A WEB-DEVELOPER    ==================
+1. Reduce 
+2. Inheritance 
+3. Prototypical Chain.
+4. Class And Object.
+ */
