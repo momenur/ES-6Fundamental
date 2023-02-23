cd3a028338bd64ebad16e77fe41e3a2f35f40a36
momenur <momenurislam6@gmail.com>
2023-02-23 15:42:28 +0600

    added map

--- fundamental.js
+++ fundamental.js
@@ -36,4 +36,33 @@ const sumThenDouble = (num1, num2) =>{
     return double;
 }
 console.log(sumThenDouble(2, 3));
+
+
  
+/*================ Map ==================
+    => map only apply on array , it make new array and can't modify the old array .
+*/
+const numbers = [2, 4, 6, 3, 10];
+const makeDouble = numbers.map(num => num * 2);
+console.log(makeDouble);
+
+// Map apply on string array
+const friends = ['Momen', 'Somrat', 'Sajal', 'Hasan', 'Humayon']; 
+const stringLength = friends.map(f => f.length);
+console.log(stringLength);
+
+// Map apply on object array
+const products = [
+    {id:1, name:'Leptop', Price:'90000'},
+    {id:2, name:'Iphone', Price:'150000'},
+    {id:3, name:'TV', Price:'50000'},
+    {id:4, name:'Power bank', Price:'5000'},
+    {id:5, name:'Mouse', Price:'2000'}
+]
+const productName = products.map(p => p.name);
+console.log(productName);
+
+
+/*================ Filter ==================
+    => map only apply on array , it make new array and can't modify the old array .
+*/
