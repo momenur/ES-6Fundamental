c4f401ccc894f9885b9caefa1cb9d0268d8abedd
momenur <momenurislam6@gmail.com>
2023-02-24 15:08:56 +0600

    added find

--- fundamental.js
+++ fundamental.js
@@ -42,16 +42,17 @@ console.log(sumThenDouble(2, 3));
 /*================ Map ==================
     => map only apply on array , it make new array and can't modify the old array .
 */
+// 1.1:
 const numbers = [2, 4, 6, 3, 10];
 const makeDouble = numbers.map(num => num * 2);
 console.log(makeDouble);
 
-// Map apply on string array
+//1.2: Map apply on string array
 const friends = ['Momen', 'Somrat', 'Sajal', 'Hasan', 'Humayon']; 
 const stringLength = friends.map(f => f.length);
 console.log(stringLength);
 
-// Map apply on object array
+//1.3: Map apply on object array
 const products = [
     {id:1, name:'Leptop', Price:'90000'},
     {id:2, name:'Iphone', Price:'150000'},
@@ -64,5 +65,30 @@ console.log(productName);
 
 
 /*================ Filter ==================
-    => map only apply on array , it make new array and can't modify the old array .
+    => Filter only apply on array , it make new array and can't modify the old array. 
 */
+// 1.1:
+const allNumbers = [20, 30, 40, 50, 25, 23, 33];
+const evenNumbers = allNumbers.filter(x => x % 2 == 0);
+console.log(evenNumbers);
+const bigNumbers = allNumbers.filter(x => x > 35);
+console.log(bigNumbers);
+
+//1.2: Filter Apply an Array of Object.
+const phones = [
+    {name: 'iPhone', RAR: '4GB', price: 150000},
+    {name: 'Onepluse', RAR: '8GB', price: 98000},
+    {name: 'Smasune', RAR: '6GB', price: 10000},
+    {name: 'Semphony', RAR: '2GB', price: 5000}
+]
+const expensive = phones.filter(phone => phone.price > 80000);
+console.log(expensive);
+
+
+/*================ Find ==================
+    => Find only apply on array , it Return a value dependents your conditions ant return the first element. 
+*/
+//1.1:
+const moreNumbers = [20, 30, 40, 50, 21];
+const findNumber = moreNumbers.find (num => num % 5 === 1);
+console.log(findNumber);
